8db9bc63c905ea8ef09af73315ff16990bab06b6
lana10-aya <lanamasood5@gmail.com>
2025-10-29 13:35:43 +0300

    add generate route

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("project1.html")
+def index():
+    data = request.args.get("data")
+    return render_template('project1.html', data=data)
 
-if __name__ =="__main__":
+@app.route('/generate',methods=["POST"])
+def generate():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        job_titile = request.form["job_title"]
+
+        response = f"{first_name} {last_name} {job_title}"
+        return redierct(url_for("index", data=response))
+
+if __name__ == '__main__' :
     app.run(debug=True)
